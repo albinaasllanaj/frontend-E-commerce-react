01da4c32deea193975c2b175040bacad7f7d20cb
Albina Asllanaj <112036955+albinaasllanaj@users.noreply.github.com>
2023-10-28 13:34:37 -0400

    Item.jsx edited

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './Item.css'
 import { Link } from 'react-router-dom'
 
-export const Item = (props) => {
+ const Item = (props) => {
   return (
     <div className='item'>
      <Link to={`/product/${props.id}`}> <img onClick={window.scrollTo(0,0)} src={props.image} alt="" /></Link>
